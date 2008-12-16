9cfc48e4ba3e012b5bd6ad84415fb405731df213
Igor Fedorenko <ifedorenko@sonatype.com>
2008-12-16 21:17:45 +0000

    Share project "modello-plugin-upgrade" into "https://svn.sonatype.org/plugins"
    git-svn-id: file:///opt/svn/repositories/sonatype.org/plugins/trunk/modello-plugin-upgrade@105 c2a14038-686d-4adc-8740-a14e5738cb64

---
+++ src/main/java/org/sonatype/modello/plugin/upgrade/AbstractConversionGenerator.java
@@ -0,0 +1,40 @@
+package org.sonatype.modello.plugin.upgrade;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+
+import org.codehaus.modello.ModelloException;
+import org.codehaus.modello.model.ModelClass;
+import org.codehaus.modello.model.Version;
+import org.codehaus.modello.plugin.AbstractModelloGenerator;
+import org.codehaus.plexus.util.WriterFactory;
+
+public abstract class AbstractConversionGenerator
+    extends AbstractModelloGenerator
+{
+
+    protected Writer getFileWriter( File dir, String name )
+        throws ModelloException
+    {
+        File f = new File( dir, name );
+
+        Writer writer;
+        try
+        {
+            writer = getEncoding() == null ? WriterFactory.newPlatformWriter( f )
+                            : WriterFactory.newWriter( f, getEncoding() );
+        }
+        catch ( IOException e )
+        {
+            throw new ModelloException( "Unable to generate: " + f + "; reason: " + e.getMessage(), e );
+        }
+        return writer;
+    }
+
+    protected static String getSourceClassName( ModelClass modelClass, Version generatedVersion )
+    {
+        return modelClass.getPackageName( true, generatedVersion ) + "." + modelClass.getName();
+    }
+
+}
